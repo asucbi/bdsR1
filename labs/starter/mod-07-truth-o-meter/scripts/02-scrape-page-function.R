448c7833b990edba25795c66f7c2ff0bc1a80826
nickduran <nicholas.duran@asu.edu>
2023-02-23 09:47:18 -0700

    completed lab 7

---
+++ labs/starter/mod-07-truth-o-meter/scripts/02-scrape-page-function.R
@@ -0,0 +1,30 @@
+# load packages ----------------------------------------------------------------
+
+library(tidyverse)
+library(rvest)
+
+# function: scrape_page --------------------------------------------------------
+
+___ <- function(url){
+  
+  # read page
+  page <- read_html(url)
+  
+  # scrape statements
+  statements <- ___
+  
+  # scrape links
+  links <- ___
+  
+  # scrape sources 
+  sources <- ___
+  
+  # scrape descriptions 
+  descriptions <- ___
+  
+  # create and return tibble
+  tibble(
+    ___
+  )
+  
+}
